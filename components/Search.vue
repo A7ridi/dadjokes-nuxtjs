8bb7e35b847f501982bad803ffa177529b74aec0
a7ridi <a7ridi@github.com>
2021-09-14 13:53:53 +0530

    dadjokes completed

---
+++ components/Search.vue
@@ -0,0 +1,44 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input
+      type="text"
+      v-model="text"
+      placeholder="Search Jokes..."
+      class="search"
+    />
+    <input type="submit" value="Search Jokes" class="submit" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchJokes',
+  data() {
+    return {
+      text: '',
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('search-text', this.text)
+      // this.text = ''
+    },
+  },
+}
+</script>
+
+<style>
+.search {
+  width: 80%;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 1px solid #444;
+}
+.submit {
+  width: 19%;
+  padding: 8px 0px;
+  border-radius: 5px;
+  border: 1px solid;
+  cursor: pointer;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <LoadingBar v-if="showHideSpinner" />
+    <AppHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader'
+import LoadingBar from '../components/LoadingBar'
+
+export default {
+  components: {
+    AppHeader,
+    LoadingBar,
+  },
+  beforeCreate() {
+    this.showHideSpinner = true
+  },
+  mounted() {
+    this.showHideSpinner = false
+  },
+  data() {
+    return {
+      showHideSpinner: true,
+    }
+  },
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  line-height: 1.6;
+  background: #f4f4f4;
+}
+a {
+  color: #666;
+  text-decoration: none;
+}
+ul {
+  list-style: none;
+}
+.container {
+  max-width: 800px;
+  margin: 2rem auto;
+  overflow: hidden;
+  padding: 1rem 2rem;
+  background: #fff;
+}
+</style>
